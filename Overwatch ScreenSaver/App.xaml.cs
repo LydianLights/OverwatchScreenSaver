d72f24af31cf6d3673812748a0deb7dd185ed528
Rane <Rane@DESKTOP-79G0N6T>
2016-08-24 15:57:16 -0700

    Add project files.

---
+++ Overwatch ScreenSaver/App.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfScreenHelper;
+
+namespace Overwatch_ScreenSaver
+{
+    //Three possible arguments
+    // /p = show screensaver preview
+    // /c = show screensaver config dialog
+    // /s = show screensaver full-screen
+
+    // /p and /c may be passed with a number representing the window handler (separated by either a space or a colon)
+    // ex /p:1234567 or /c 1234567
+
+    //If no arguments behave as if /c was passed
+
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            //store command line arguments
+            string[] args = e.Args;
+
+            //if no arguments given, treat like /s
+            if (args.Length == 0)
+            {
+                args = new string[] { "/s" };
+            }
+
+
+            //format first argument
+            string firstArgument = args[0].ToLower().Trim();
+            string secondArgument = null;
+
+            //handle arguments separated by a colon
+            if (firstArgument.Length > 2)
+            {
+                secondArgument = firstArgument.Substring(3).Trim();
+                firstArgument = firstArgument.Substring(0, 2);
+            }
+            else if (args.Length > 1)
+            {
+                secondArgument = args[1];
+            }
+
+            if (firstArgument == "/c")          //Configuration mode
+            {
+                RunConfig();
+            }
+            else if (firstArgument == "/p")     //Preview mode
+            {
+                RunPreview();
+            }
+            else if (firstArgument == "/s")     //Fullscreen mode
+            {
+                RunScreenSaver();
+            }
+            else        //Invalid argument
+            {
+                MessageBox.Show("Invalid command line argument '" + firstArgument + "'", "Error", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+        }
+
+        private void RunScreenSaver()
+        {
+            //create the screensaver in each monitor
+            foreach (Screen screen in Screen.AllScreens)
+            {
+                ScreenSaverWindow wnd = new ScreenSaverWindow(screen.Bounds);
+                wnd.Show();
+            }
+        }
+
+        private void RunPreview()
+        {
+            //TODO
+            this.Shutdown();
+        }
+
+        private void RunConfig()
+        {
+            //TODO
+            MessageBox.Show("NERF THIS", ";)", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            this.Shutdown();
+        }
+    }
+}
---
+++ Overwatch ScreenSaver/Windows/ScreenSaverWindow.xaml.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Overwatch_ScreenSaver
+{
+    public partial class ScreenSaverWindow : Window
+    {
+        #region Initialization
+        //initialize screensaver to screen bounds
+        public ScreenSaverWindow(Rect bounds)
+        {
+            InitializeComponent();
+            this.Left = bounds.Left;
+            this.Top = bounds.Top;
+            this.Width = bounds.Width;
+            this.Height = bounds.Height;
+
+            //set up timer for animation
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(40);
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+
+        //make the screensaver be far even as decided to use even go want to do look more like
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.Cursor = Cursors.None;
+            this.Topmost = true;
+        }
+        #endregion
+
+
+        #region Input Detection
+        //close the screensaver when user input is detected
+        Point mousePrevLocation;
+        Point mouseCurrLocation;
+        readonly int mouseMoveTolerance = 3;
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            //quit if mouse is moved a significant amount
+            if (!Point.Equals(mousePrevLocation, new Point(0,0)))
+            {
+                mouseCurrLocation = e.GetPosition(this);
+
+                if (Math.Abs(mousePrevLocation.X - mouseCurrLocation.X) > mouseMoveTolerance ||
+                    Math.Abs(mousePrevLocation.Y - mouseCurrLocation.Y) > mouseMoveTolerance)
+                {
+                    Application.Current.Shutdown();
+                }
+            }
+
+            mousePrevLocation = e.GetPosition(this);
+        }
+        #endregion
+
+
+        #region Animation
+        //spawn images and move them around based on the timer
+        List<Image> imageList = new List<Image>();
+        Vector velocity = new Vector(-1, 1);
+        Random r = new Random();
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            MoveImages();
+            if (r.Next(100) == 0)
+            {
+                AddImage();
+            }
+        }
+
+        //spawn a new image and add it to the image list
+        private void AddImage()
+        {
+            Image image = new Image();
+            image.Source = Application.Current.FindResource("DVa") as BitmapImage;
+
+            double scale = r.NextDouble() + 0.5;
+            //scale = 1;
+            image.Width = 276 * scale;
+            //double.NaN means "auto"
+            //image.Height = double.NaN;
+            image.Height = 287.5 * scale;
+
+            //choose a point to spawn at along the top or right border
+            int nextSpawn = r.Next((int)(this.Width + this.Height));
+
+            Point location;
+            //check if the top border was chosen
+            if (nextSpawn < this.Width)
+            {
+                location = new Point(nextSpawn, -image.Height);
+            }
+            //else spawn on right border
+            else
+            {
+                location = new Point(this.Width, nextSpawn - this.Width - image.Height);
+            }
+
+            image.SetValue(Canvas.LeftProperty, location.X);
+            image.SetValue(Canvas.TopProperty, location.Y);
+
+            imageLayer.Children.Add(image);
+            imageList.Add(image);
+        }
+
+        //cycle through the image list and move them based on global velocity
+        private void MoveImages()
+        {
+            for (int i = 0; i < imageList.Count; i++)
+            {
+                Image image = imageList[i];
+                double x = (double)image.GetValue(Canvas.LeftProperty) + velocity.X;
+                double y = (double)image.GetValue(Canvas.TopProperty) + velocity.Y;
+
+                image.SetValue(Canvas.LeftProperty, x);
+                image.SetValue(Canvas.TopProperty, y);
+
+                if (x < -image.Width || y > this.Height)
+                {
+                    RemoveImage(i);
+                    i--;
+                }
+            }
+        }
+
+        private void RemoveImage(int index)
+        {
+            imageLayer.Children.RemoveAt(index);
+            imageList.RemoveAt(index);
+        }
+        #endregion
+    }
+}
