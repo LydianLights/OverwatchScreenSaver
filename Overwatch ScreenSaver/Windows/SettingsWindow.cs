495309ef1ce097d7e0c1acf6530bd21f0046f330
Rane <rane.fields@gmail.com>
2016-08-27 21:20:54 -0700

    Added a placeholder for the setting window

---
+++ Overwatch ScreenSaver/Windows/SettingsWindow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OverwatchScreenSaver.Windows
+{
+    public partial class SettingsWindow : Form
+    {
+        public SettingsWindow()
+        {
+            InitializeComponent();
+
+            //TODO
+        }
+    }
+}
